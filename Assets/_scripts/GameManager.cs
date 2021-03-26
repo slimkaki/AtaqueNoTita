e208a6ef100808f1e0b1ea1adf9b08a4197683ff
lucaslealvale <lucaslealvale01@gmail.com>
2021-03-26 16:33:45 -0300

    comecando

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class GameManager 
+// {
+//    private static GameManager _instance;
+//    public enum GameState { MENU, GAME, PAUSE, ENDGAME };
+
+//    public GameState gameState { get; private set; }
+//    public int vidas;
+//    public int pontos;
+//    public int time;
+//    public  Vector3 position;
+//    public int nivelArma;
+//    public static GameManager GetInstance()
+//    {
+//        if(_instance == null)
+//        {
+//            _instance = new GameManager();
+//        }
+
+//        return _instance;
+//    }
+//    private GameManager()
+//    {
+//        vidas = 10;
+//        pontos = 0;
+//        time = 6000;
+//        nivelArma = 0;
+
+//        position = new Vector3(-5.25f,-4.0f,0);
+//        gameState = GameState.MENU;
+//    }
+//     public delegate void ChangeStateDelegate();
+
+//     public static ChangeStateDelegate changeStateDelegate;
+
+//     public void ChangeState(GameState nextState)
+//     {
+
+//        if (nextState == GameState.MENU) Reset();
+        
+//         if(gameState == GameState.PAUSE && nextState == GameState.MENU){
+//             GameObject.FindWithTag("Player").transform.position = position;
+//         }
+        
+//         gameState = nextState;
+        
+//         changeStateDelegate();
+        
+//     }
+
+//     private void Reset()
+//     {
+//     vidas = 10;
+//     pontos = 0;
+//     time = 6000;
+//     nivelArma = 0;
+//     }
+// }
---
+++ Assets/_scripts/playerControler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+    // GameManager gm;
+    private float positionX, positionY;
+    private bool jumpFlag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // gm = GameManager.GetInstance();
+        positionX = this.transform.position.x;
+        positionY = this.transform.position.y;
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jumpFlag) {
+            positionY += 0.01f;
+        }                
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            positionX -= 0.01f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            positionX += 0.01f;
+        }
+        if (Input.GetAxisRaw("Jump") != 0){
+            jumpFlag = true;
+        }
+        
+        
+        
+
+        this.transform.position = new Vector3(positionX, positionY, this.transform.position.z);
+    }
+    
+}
