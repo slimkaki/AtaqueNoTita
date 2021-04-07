6dce6d49f636bfb3032e9ae7f5850cede0751975
slimkaki <rafaalmada98@gmail.com>
2021-04-07 17:47:43 -0300

    pulo resolvido e comecando titã
    Co-authored-by: Lucas <lucaslealvale01@gmail.com>

---
+++ Assets/_scripts/TitanController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitanController : MonoBehaviour {
+    public Collider2D collider;
+    void Start() {
+        
+    }
+
+
+    void Update() {
+        
+    }
+}
--- Assets/_scripts/playerControler.cs
+++ Assets/_scripts/playerControler.cs
@@ -8,48 +8,69 @@ public class playerControler : MonoBehaviour
     private float positionX, positionY;
     public float speed;
     public float jumpVelocity;
-    public Rigidbody2D rb;
-    private float lastHorizontal = 0.0f;
+    private Rigidbody2D rb;
+    private float lastHorizontal = 1.0f;
+    public bool isGrounded = false;
+    private bool isOnAir = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() 
+    {   
         // gm = GameManager.GetInstance();
+        rb = GetComponent<Rigidbody2D>();
         positionX = this.transform.position.x;
         positionY = this.transform.position.y;
-        
     }
 
     void Jump() {
-        rb.AddForce(Vector2.up*jumpVelocity, ForceMode2D.Impulse);
-        
+        // rb.AddForce(Vector2.up*jumpVelocity, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x, 10);
+        isOnAir = true;
+    }
+    void Impulse() {
+        // rb.AddForce(Vector2.up*jumpVelocity, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x*9, 16 );
+        isOnAir = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {         
-        if (Input.GetAxisRaw("Jump") != 0) {
+        if (Input.GetAxisRaw("Vertical") > 0f && isGrounded) {
             Jump();
-        }
-        
+        } 
+        if (Input.GetAxisRaw("Jump") > 0f && isOnAir) {
+            Impulse();
+        } 
+          
         float h = Input.GetAxisRaw("Horizontal");
         float horizontal = h * speed * Time.deltaTime;
+
         if ((lastHorizontal == 1.0f && h == -1.0f) || (lastHorizontal == -1.0f && h == 1.0f)) {
             this.transform.Rotate(this.transform.rotation.x, -180f, this.transform.rotation.z);
-        } 
+        }
+
         this.transform.position = new Vector3(this.transform.position.x + horizontal, this.transform.position.y, this.transform.position.z);
         if (h != 0.0f && lastHorizontal != h) {
             lastHorizontal = h;
         }
     }
-
   
-    // }
 
-    // void OnTriggerEnter2D(Collider2D collision) {
-    //     if (collision.CompareTag("roofTop")) {// && !jumpFlag) {
-    //         jumpFlag = false;
-    //     }
-    // }
+    void OnCollisionEnter2D(Collision2D collision) {        
+        Debug.Log($"collision.collider.name: {collision.collider.name}");
+        if(collision.gameObject.tag == "roofTop")
+        {
+        this.isGrounded = true;
+        }
+    } 
+ 
+    void OnCollisionExit2D(Collision2D collision) {
+        if(collision.gameObject.tag == "roofTop")
+        {
+        this.isGrounded = false;
+        }
+        }
     
 }
-
+ 
